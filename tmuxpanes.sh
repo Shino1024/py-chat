dcb64eeb84840612425f1e4b864d63414a2acef4
Shino1024 <justincompspec@gmail.com>
2015-08-17 00:28:00 +0200

    Another fix.

--- tmuxpanes.sh
+++ tmuxpanes.sh
@@ -8,6 +8,7 @@ if [[ $PORT =~ ^([1-9][0-9]{,3}|[1-6][0-9]{,4})$ ]]; then
 	if [ "$PORT" -lt 1024 ] || [ "$PORT" -gt 65536 ]; then
 		echo "The port should be between 1024 and 65536."
 		return 1
+	fi
 else
 	echo "The port should be a number between 1024 and 65536."
 	return 1
